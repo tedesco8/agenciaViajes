720b862502c6a44dcbb323bee7f67812def1ccd6
Pablo Tedesco <tedesco8@hotmail.com>
2019-07-11 14:24:23 -0300

    creando las vistas

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 //importar express
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 //configuracion express
 const app = express();
+//habilitar pug
+app.set('view engine', 'pug');
+
+//anadir las vistas
+app.set('views', path.join(__dirname, './views'));
+
 //cargar las rutas
 app.use('/', routes());
 
