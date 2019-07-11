43c5ce4a6818fd1a279ac90a6d57cdafc6e80a96
Pablo Tedesco <tedesco8@hotmail.com>
2019-07-11 14:44:46 -0300

    creando directorio para archivos estaticos

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ app.set('view engine', 'pug');
 //anadir las vistas
 app.set('views', path.join(__dirname, './views'));
 
+//cargar carpeta estatica public
+app.use(express.static('public'));
+
 //cargar las rutas
 app.use('/', routes());
 
